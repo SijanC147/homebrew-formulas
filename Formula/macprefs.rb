40d4c0cb072e4c0cf8aceee8fa6bea0b3c4cf10b
Sean B <seanbugeja23@gmail.com>
2023-07-28 19:34:25 +0200

    Updating to version v2.0.2

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -1,18 +1,18 @@
 class Macprefs < Formula
-    include Language::Python::Virtualenv
+  include Language::Python::Virtualenv
 
-    desc "Backup and Restore your Mac System and App Preferences"
-    homepage "https://github.com/sijanc147/macprefs"
-    url "https://github.com/sijanc147/macprefs/archive/v2.0.1.tar.gz"
-    sha256 "1250fa7627fdbf0ecdeacbcaadd1de5121a68f8128db35c44eebca92128308bb"
+  desc "Backup and Restore your Mac System and App Preferences"
+  homepage "https://github.com/sijanc147/macprefs"
+  url "https://github.com/sijanc147/macprefs/archive/v2.0.2.tar.gz"
+  sha256 "73a07c446c0acde8bd12f36f9da26f030785ad374db2bf90143e54b0aace6ee2"
 
-    depends_on :python3
+  depends_on "python3"
 
-    def install
-      bin.install Dir["*"]
-    end
-
-    test do
-      system "#{bin}/macprefs", "--help"
-    end
+  def install
+    bin.install Dir["*"]
   end
+
+  test do
+    system "#{bin}/macprefs", "--help"
+  end
+end
