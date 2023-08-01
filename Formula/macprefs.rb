a2607b85731d7bd0b0bf3f83a802b0304549e9a5
Sean Bugeja <seanbugeja23@gmail.com>
2023-08-01 13:11:22 +0000

    Auto update version and sha256 for release v2.0.14

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -3,13 +3,18 @@ class Macprefs < Formula
 
   desc "Backup and Restore your Mac System and App Preferences"
   homepage "https://github.com/sijanc147/macprefs"
-  url "https://github.com/sijanc147/macprefs/archive/v2.0.2.tar.gz"
-  sha256 "73a07c446c0acde8bd12f36f9da26f030785ad374db2bf90143e54b0aace6ee2"
+  url "https://api.github.com/repos/SijanC147/macprefs/releases/download/v2.0.14/macprefs-2.0.14.tar.gz"
+  sha256 "af8737725537718fa7b3726a891fc51041f5bf3610060049553b5bc53a441446"
 
-  depends_on "python3"
+  depends_on "python"
+
+  resource "mock" do
+    url "https://files.pythonhosted.org/packages/66/ab/41d09a46985ead5839d8be987acda54b5bb93f713b3969cc0be4f81c455b/mock-5.1.0.tar.gz"
+    sha256 "5e96aad5ccda4718e0a229ed94b2024df75cc2d55575ba5762d31f5767b8767d"
+  end
 
   def install
-    bin.install Dir["*"]
+    virtualenv_install_with_resources
   end
 
   test do
