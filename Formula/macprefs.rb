8017d5267e2ff167096355f78935050b42199e6c
Sean Bugeja <seanbugeja23@gmail.com>
2023-08-01 16:59:00 +0000

    Auto update version and sha256 for release v2.0.26

--- Formula/macprefs.rb
+++ Formula/macprefs.rb
@@ -3,8 +3,8 @@ class Macprefs < Formula
 
   desc "Backup and Restore your Mac System and App Preferences"
   homepage "https://github.com/sijanc147/macprefs"
-  url "https://github.com/SijanC147/macprefs/releases/download/v2.0.21/macprefs-2.0.21.tar.gz"
-  sha256 "2efd8cf2341d41cceb2e5af014004f53808c5967fadeba6e0c9b1ce14c7971d6"
+  url "https://github.com/SijanC147/macprefs/releases/download/v2.0.26/macprefs-2.0.26.tar.gz"
+  sha256 "23cd15a6ee4490a433575f51f02e097faa3c69fa17e511d9a21faa387a134bdb"
 
   depends_on "python"
 
