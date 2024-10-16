e1a731f7ff563a378f972ed1def3463cfae8da01
homebrew-releaser <homebrew-releaser@ghaction.com>
2024-10-16 23:25:55 +0000

    chore: brew formula update for pure v1.23.9

--- Formula/pure.rb
+++ Formula/pure.rb
@@ -5,8 +5,8 @@
 class Pure < Formula
   desc "Pretty, minimal and fast zsh prompt"
   homepage "https://github.com/SijanC147/pure"
-  url "https://github.com/SijanC147/pure/archive/refs/tags/v1.23.8.tar.gz"
-  sha256 "4bd150b0b9920a1bca2342a4c043638ba8b5fd8f3b926bb0c7f77cd6bea9ce33"
+  url "https://github.com/SijanC147/pure/archive/refs/tags/v1.23.9.tar.gz"
+  sha256 "22598ea17918636439786d0af119cff13b1f0539f6227d66f6e1b45f552b9729"
   license "MIT"
 
   depends_on "zsh" => :test
