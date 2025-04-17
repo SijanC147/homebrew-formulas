d31eb18239dad055aa41e44d7ea7dbb7db3daa69
Sean Bugeja <seanbugeja23@gmail.com>
2025-04-17 12:43:24 +0200

    update redpanda

--- Formula/redpanda-console.rb
+++ Formula/redpanda-console.rb
@@ -1,8 +1,8 @@
 class RedpandaConsole < Formula
   desc "Redpanda Console is a developer-friendly UI for managing your Kafka/Redpanda workloads."
   homepage "https://github.com/redpanda-data/console"
-  url "https://github.com/redpanda-data/console/releases/download/v2.3.5/redpanda_console_2.3.5_darwin_arm64.tar.gz"
-  version "2.3.5"
+  url "https://github.com/redpanda-data/console/releases/download/v3.0.0/redpanda_console_3.0.0_darwin_arm64.tar.gz"
+  version "3.0.0"
 
   # sha256 "2327e84d70ce9865638914ce805f1a12e03ad4de661918a8244847a118970e28"
 
